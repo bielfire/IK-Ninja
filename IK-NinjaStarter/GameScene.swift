1ff4ec31db9a2c953ef4c8c662a9f295220994b4
Gabriel moreira <bielfire@gmail.com>
2019-12-02 20:33:49 -0300

    Definido uma ação de cinemática inversa; Configurado um efeito final; Definido restrições conjuntas; Criado o movimento de perfuração; Socando com os dois punhos; Enfrentando o alvo; Feito rastreamento com a cabeça;  Atingindo alvos móveis;

--- IK-NinjaStarter/GameScene.swift
+++ IK-NinjaStarter/GameScene.swift
@@ -24,17 +24,30 @@ import SpriteKit
 
 class GameScene: SKScene {
     
-    // - Properties
+    // MRK: - Properties
     
     var shadow: SKNode!
     var lowerTorso: SKNode!
     var upperTorso: SKNode!
     var upperArmFront: SKNode!
     var lowerArmFront: SKNode!
+    var fistFront: SKNode!
+    var upperArmBack: SKNode!
+    var lowerArmBack: SKNode!
+    var fistBack: SKNode!
+    var rightPunch = true
+    var head: SKNode!
+    var firstTouch = false
+    var lastSpawnTimeInterval: TimeInterval = 0
+    var lastUpdateTimeInterval: TimeInterval = 0
+    let upperArmAngleDeg: CGFloat = -10
+    let lowerArmAngleDeg: CGFloat = 130
+    let targetNode = SKNode()
     
-    // - Override
+    // MARK: - Override
     
     override func didMove(to view: SKView) {
+        let rotationConstraintArm = SKReachConstraints(lowerAngleLimit: CGFloat(0), upperAngleLimit: CGFloat(160))
         lowerTorso = childNode(withName: "torso_lower")
         lowerTorso.position = CGPoint(x: frame.midX, y: frame.midY - 30)
         
@@ -44,17 +57,160 @@ class GameScene: SKScene {
         upperTorso = lowerTorso.childNode(withName: "torso_upper")
         upperArmFront = upperTorso.childNode(withName: "arm_upper_front")
         lowerArmFront = upperArmFront.childNode(withName: "arm_lower_front")
-    }
-    
-    func punchAt(_ location: CGPoint) {
-        let punch = SKAction.reach(to: location, rootNode: upperArmFront, duration: 0.1)
-        lowerArmFront.run(punch)
+        
+        fistFront = lowerArmFront.childNode(withName: "fist_front")
+        lowerArmFront.reachConstraints = rotationConstraintArm
+        
+        upperArmBack = upperTorso.childNode(withName: "arm_upper_back")
+        lowerArmBack = upperArmBack.childNode(withName: "arm_lower_back")
+        
+        fistBack = lowerArmBack.childNode(withName: "fist_back")
+        lowerArmBack.reachConstraints = rotationConstraintArm
+        
+        head = upperTorso.childNode(withName: "head")
+        
+        
+        let orientToNodeConstraint = SKConstraint.orient(to: targetNode, offset: SKRange(constantValue: 0.0))
+        let range = SKRange(lowerLimit: CGFloat(-50).degreesToRadians(),
+                            upperLimit: CGFloat(80).degreesToRadians())
+        let rotationConstraint = SKConstraint.zRotation(range)
+        rotationConstraint.enabled = false
+        orientToNodeConstraint.enabled = false
+        head.constraints = [orientToNodeConstraint, rotationConstraint]
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if !firstTouch {
+            for c in head.constraints! {
+                let constraint = c
+                constraint.enabled = true
+            }
+            
+            firstTouch = true
+        }
+        
         for touch: AnyObject in touches {
             let location = touch.location(in: self)
+            lowerTorso.xScale =
+                location.x < frame.midX ? abs(lowerTorso.xScale) * -1 : abs(lowerTorso.xScale)
             punchAt(location)
+            targetNode.position = location
         }
+    }
+    
+    // MARK: - Methods
+    
+    func punchAt(_ location: CGPoint, upperArmNode: SKNode, lowerArmNode: SKNode, fistNode: SKNode) {
+        let punch = SKAction.reach(to: location, rootNode: upperArmNode, duration: 0.1)
+        let restore = SKAction.run {
+            let rotateActionA = SKAction.rotate(toAngle: self.upperArmAngleDeg.degreesToRadians(), duration: 0.1)
+            let rotateActionB = SKAction.rotate(toAngle: self.lowerArmAngleDeg.degreesToRadians(), duration: 0.1)
+            upperArmNode.run(rotateActionA)
+            lowerArmNode.run(rotateActionB)
+        }
+        
+        let checkIntersection = intersectionCheckAction(for: fistNode)
+        fistNode.run(SKAction.sequence([punch, checkIntersection, restore]))
+    }
+    
+    func punchAt(_ location: CGPoint) {
+        if rightPunch {
+            punchAt(location, upperArmNode: upperArmFront, lowerArmNode: lowerArmFront, fistNode: fistFront)
+        }
+            
+        else {
+            punchAt(location, upperArmNode: upperArmBack, lowerArmNode: lowerArmBack, fistNode: fistBack)
+        }
+        
+        rightPunch = !rightPunch
+    }
+    
+    func addShuriken() {
+        let shuriken = SKSpriteNode(imageNamed: "projectile")
+        
+        let minY = lowerTorso.position.y - 60 + shuriken.size.height/2
+        let maxY = lowerTorso.position.y  + 140 - shuriken.size.height/2
+        let rangeY = maxY - minY
+        let actualY = CGFloat(arc4random()).truncatingRemainder(dividingBy: rangeY) + minY
+        
+        let left = arc4random() % 2
+        let actualX = (left == 0) ? -shuriken.size.width/2 : size.width + shuriken.size.width/2
+        
+        shuriken.position = CGPoint(x: actualX, y: actualY)
+        shuriken.name = "shuriken"
+        shuriken.zPosition = 1
+        addChild(shuriken)
+        
+        let minDuration = 4.0
+        let maxDuration = 6.0
+        let rangeDuration = maxDuration - minDuration
+        let actualDuration = Double(arc4random()).truncatingRemainder(dividingBy: rangeDuration) + minDuration
+        
+        let actionMove = SKAction.move(to: CGPoint(x: size.width/2, y: actualY), duration: actualDuration)
+        let actionMoveDone = SKAction.removeFromParent()
+        shuriken.run(SKAction.sequence([actionMove, actionMoveDone]))
+        
+        let angle = left == 0 ? CGFloat(-90).degreesToRadians() : CGFloat(90).degreesToRadians()
+        let rotate = SKAction.repeatForever(SKAction.rotate(byAngle: angle, duration: 0.2))
+        shuriken.run(SKAction.repeatForever(rotate))
+    }
+    
+    func updateWithTimeSinceLastUpdate(timeSinceLast: CFTimeInterval) {
+        lastSpawnTimeInterval = timeSinceLast + lastSpawnTimeInterval
+        if lastSpawnTimeInterval > 0.75 {
+            lastSpawnTimeInterval = 0
+            addShuriken()
+        }
+    }
+    
+    override func update(_ currentTime: CFTimeInterval) {
+        var timeSinceLast = currentTime - lastUpdateTimeInterval
+        lastUpdateTimeInterval = currentTime
+        if timeSinceLast > 1.0 {
+            timeSinceLast = 1.0 / 60.0
+            lastUpdateTimeInterval = currentTime
+        }
+        
+        updateWithTimeSinceLastUpdate(timeSinceLast: timeSinceLast)
+    }
+    
+    func intersectionCheckAction(for effectorNode: SKNode) -> SKAction {
+        let checkIntersection = SKAction.run {
+            
+            for object: AnyObject in self.children {
+                // check for intersection against any sprites named "shuriken"
+                if let node = object as? SKSpriteNode {
+                    if node.name == "shuriken" {
+                        // convert coordinates into common system based on root node
+                        let effectorInNode = self.convert(effectorNode.position, from:effectorNode.parent!)
+                        var shurikenFrame = node.frame
+                        shurikenFrame.origin = self.convert(shurikenFrame.origin, from: node.parent!)
+                        
+                        if shurikenFrame.contains(effectorInNode) {
+                            // play a hit sound
+                            self.run(SKAction.playSoundFileNamed("hit.mp3", waitForCompletion: false))
+                            
+                            // show a spark effect
+                            let spark = SKSpriteNode(imageNamed: "spark")
+                            spark.position = node.position
+                            spark.zPosition = 60
+                            self.addChild(spark)
+                            let fadeAndScaleAction = SKAction.group([SKAction.fadeOut(withDuration: 0.2), SKAction.scale(to: 0.1, duration: 0.2)])
+                            let cleanUpAction = SKAction.removeFromParent()
+                            spark.run(SKAction.sequence([fadeAndScaleAction, cleanUpAction]))
+                            
+                            // remove the shuriken
+                            node.removeFromParent()
+                        }
+                        else {
+                            // play a miss sound
+                            self.run(SKAction.playSoundFileNamed("miss.mp3", waitForCompletion: false))
+                        }
+                    }
+                }
+            }
+        }
+        
+        return checkIntersection
     }
 }
